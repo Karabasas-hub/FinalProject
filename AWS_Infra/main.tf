9875e10fbada1d8df732eb9a0e6d1300c1e66681
Karabasas-hub <ainasau33@gmail.com>
2025-01-21 21:48:51 +0200

    Update main.tf

--- AWS_Infra/main.tf
+++ AWS_Infra/main.tf
@@ -16,7 +16,7 @@ terraform {
     }
 }
 
-resource "aws_dynamodb_table" "api_data_table" {
+resource "aws_dynamodb_table" "Tasks" {
     name         = "api-tasks-table"
     billing_mode = "PAY_PER_REQUEST"
     hash_key     = "id"
